7922072ff481c922eb53aa1edf82135ccd61c218
20211TIIMI0168 <gabrieldpbrunetti@gmail.com>
2023-04-26 12:56:17 -0300

    commit inicial

---
+++ app/src/main/java/brunetti/depaula/galeria/PhotoActivity.java
@@ -0,0 +1,27 @@
+package brunetti.depaula.galeria;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
