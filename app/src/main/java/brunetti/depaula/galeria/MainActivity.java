0cfc0a49e300a74f35ab30f00dd155b8cdfabef6
20211TIIMI0168 <gabrieldpbrunetti@gmail.com>
2023-05-03 12:50:27 -0300

    commit inicial

--- app/src/main/java/brunetti/depaula/galeria/MainActivity.java
+++ app/src/main/java/brunetti/depaula/galeria/MainActivity.java
@@ -4,13 +4,33 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int RESULT_TAKE_PICTURE = 1;
+    String currentPhotoPath;
+    List<String> photos = new ArrayList<>();
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            photos.add(files[i].getAbsolutePath());
+        }
+
+        mainAdapter = new MainAdapter(MainActivity.this, photos);
+        RecyclerView rvGalery = findViewById(R.id.rvGalery);
+        rvGalery.setAdapter(mainAdapter);
+
+        float w = getResources().getDimension(R.dimen.itemWidth);
+        int numberofColumns = Util.calculateNoOfColumns(MainActivity.this, w);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, numberofColumns);
+        rvGalery.setLayoutManager(gridLayoutManager);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_tb, menu);
@@ -33,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        switch(item.getItemId()){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.opCamera:
                 dispatchTakePictureIntent();
                 return true;
@@ -42,4 +77,39 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    public void startPhotoActivity(String photoPath){
+        Intent i = new Intent(MainActivity.this, PhotoActivity.class);
+        i.putExtra("photo_path", photoPath);
+        startActivity(i);
+    }
+
+    private void dispatchTakePictureIntent(){
+        File f = null;
+        try{
+            f = createImageFile();
+        }
+        catch(IOException e){
+            Toast.makeText(MainActivity.this, "Não foi possível criar o arquivo", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        currentPhotoPath = f.getAbsolutePath();
+
+        if(f != null){
+            Uri fUri = FileProvider.getUriForFile(MainActivity.this, "brunetti.depaula.galeria.fileprovider", f);
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, fUri);
+        startActivityForResult(i, RESULT_TAKE_PICTURE);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    File f = File.createTempFile(imageFileName, ".jpg", storageDir);
+    return f;
+    }
+
 }
--- app/src/main/java/brunetti/depaula/galeria/PhotoActivity.java
+++ app/src/main/java/brunetti/depaula/galeria/PhotoActivity.java
@@ -3,15 +3,26 @@ package brunetti.depaula.galeria;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class PhotoActivity extends AppCompatActivity {
 
+    String photoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+
+        Bitmap bitmap = Util.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
     }
 
     @Override
