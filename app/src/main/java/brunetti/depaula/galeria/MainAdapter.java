cc414282b9195a74ca95a40b5e9fcce2331277cc
20211TIIMI0168 <gabrieldpbrunetti@gmail.com>
2023-05-19 10:25:07 -0300

    comentario parcial

--- app/src/main/java/brunetti/depaula/galeria/MainAdapter.java
+++ app/src/main/java/brunetti/depaula/galeria/MainAdapter.java
@@ -24,6 +24,7 @@ public class MainAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
+    //inicializa um viewHolder que ira inflar o layout de list_item
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(mainActivity);//Inicializa o LayoutInflater que recebe o mainActivity e infla os elementos em tempo de execução
         View v = inflater.inflate(R.layout.list_tem,parent,false);//Inicializa os elementos view a partir do LayoutInflater que gerou os elementos View a partir do arquivo xml MainActivity
@@ -32,12 +33,17 @@ public class MainAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position){
+        //acessa o imageView do layout list_item
         View v = holder.itemView;
         ImageView imPhoto = v.findViewById(R.id.imItem);
+        //define as dimensoes da foto
         int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
         int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
+        //cria um bitmap dado um uri e as dimensoes
         Bitmap bitmap = Util.getBitmap(photos.get(position), w, h);
         imPhoto.setImageBitmap(bitmap);
+        //adiciona um um listener de click para as imagens
+        //este listener inicializa a photoActivity quando a foto e clicada
         imPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +53,7 @@ public class MainAdapter extends RecyclerView.Adapter {
     }
 
     @Override
+    //retorna o tamanho do array de fotos
     public int getItemCount() {
         return photos.size();
     }
--- app/src/main/java/brunetti/depaula/galeria/PhotoActivity.java
+++ app/src/main/java/brunetti/depaula/galeria/PhotoActivity.java
@@ -23,23 +23,31 @@ public class PhotoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //inicializa a tela de layout de activity_photo
         setContentView(R.layout.activity_photo);
 
+        //inicializa uma toolbar para a tela
         Toolbar toolbar = findViewById(R.id.tbPhoto);
         setSupportActionBar(toolbar);
 
+        //inicializa uma ActionBar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        //captura a intent que iniciou a activity
         Intent i = getIntent();
+        //captura o uri da foto clicada
         photoPath = i.getStringExtra("photo_path");
 
+        //define um bitmap dado um uri de um foto
         Bitmap bitmap = Util.getBitmap(photoPath);
+        //define o bitmap do image view
         ImageView imPhoto = findViewById(R.id.imPhoto);
         imPhoto.setImageBitmap(bitmap);
     }
 
     @Override
+    //se o icone de compartilhar for clicado inicia a funcao sharePhoto
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         switch (item.getItemId()){
             case R.id.opShare:
@@ -51,6 +59,7 @@ public class PhotoActivity extends AppCompatActivity {
     }
 
     @Override
+    //inicializa o layout da toolbar
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
@@ -59,10 +68,15 @@ public class PhotoActivity extends AppCompatActivity {
     }
 
     void sharePhoto(){
+        //cria um uri para um arquivo dado
         Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this, "brunetti.depaula.galeria.fileprovider", new File(photoPath));
+        //inicializa uma intent para enviar enviar dados
         Intent i = new Intent(Intent.ACTION_SEND);
+        //adiciona o uri que sera enviado
         i.putExtra(Intent.EXTRA_STREAM, photoUri);
+        //define o tipo de dado que a intent ira receber
         i.setType("image/jpeg");
+        //iniciliza a intent
         startActivity(i);
     }
 }
